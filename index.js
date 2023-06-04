7416b5871ef44fb84dc4379f59a148c3ca0afbc1
leandroalfonso <68873104+leandroalfonso@users.noreply.github.com>
2023-06-03 21:27:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-const multer = require('multer');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + '-' + file.originalname);
-  }
-});
-
-const upload = multer({ storage: storage });
 
 const connection = mysql.createConnection({
   host: 'bcjyxukjdqqjhqx76bgm-mysql.services.clever-cloud.com',
@@ -34,18 +21,17 @@ app.get('/usuarios', (req, res) => {
   });
 });
 
-app.post('/usuarios', upload.single('foto_usuario'), (req, res) => {
+app.post('/usuarios', (req, res) => {
   const {
     nome_usuario,
     profissao_usuario,
     endereco_usuario,
     habilidades,
+    foto_usuario,
     ajudador,
     preciso_ser_ajudado,
     mora_aonde
   } = req.body;
-
-  const foto_usuario = req.file.filename;
 
   const query = `INSERT INTO usuarios (nome_usuario, profissao_usuario, endereco_usuario, habilidades, foto_usuario, ajudador, preciso_ser_ajudado, mora_aonde)
                  VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
