4531553fca937a0c9d650cd9f2758d6fe7ca59f0
leandro <leandro.meira.alfonso@hotmail.com>
2023-07-05 23:22:54 -0300

    tirado alert

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ app.post('/usuarios', async (req, res) => {
     const [results] = await connection.query(query, values);
     connection.release();
     res.json({ message: 'Usuário inserido com sucesso!' });
-    res.send('<script>alert("Usuário inserido com sucesso!!"); window.location.href = "index.html";</script>');
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Ocorreu um erro ao inserir o usuário.' });
@@ -69,7 +68,7 @@ app.get('/usuarios/:id', async (req, res) => {
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Ocorreu um erro ao buscar o usuário..' });
+    res.status(500).json({ error: 'Ocorreu um erro ao buscar o usuário.' });
   }
 });
 
